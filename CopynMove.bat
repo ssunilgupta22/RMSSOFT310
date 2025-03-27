b4053b23c5231376be54ecf2ea1a66f870500718
Sunil Gupta <ssunilgupta22@yahoo.co.in>
2025-03-27 19:20:39 +0530

    Add files via upload

---
+++ CopynMove.bat
@@ -0,0 +1,24 @@
+<<<<<<< HEAD
+
+    @echo off
+
+    echo %time%
+    timeout 5 > NUL
+    echo %time%
+
+    move  F:\RMSSOFT310\mrms_pro.exe F:\RMSSOFT310\win_rms\RMS_BACKUP 
+    copy F:\RMSSOFT310\MyfileDownload\mrms_pro.exe F:\RMSSOFT310\mrms_pro.exe 
+    start F:\RMSSOFT310\mrms_pro.exe 
+=======
+
+    @echo off
+
+    echo %time%
+    timeout 5 > NUL
+    echo %time%
+
+    move  F:\RMSSOFT310\mrms_pro.exe F:\RMSSOFT310\win_rms\RMS_BACKUP 
+    copy F:\RMSSOFT310\MyfileDownload\mrms_pro.exe F:\RMSSOFT310\mrms_pro.exe 
+    start F:\RMSSOFT310\mrms_pro.exe 
+>>>>>>> 03ee8641c58ed4e4c31fa761e893c2c6a73b20d2
+   exit /b
