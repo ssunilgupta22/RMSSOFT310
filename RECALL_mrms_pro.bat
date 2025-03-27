d379cc48604b7bcfed6d18049c6e77f0fc271d3f
Sunil Gupta <ssunilgupta22@yahoo.co.in>
2025-03-27 19:23:40 +0530

    Add files via upload

---
+++ RECALL_mrms_pro.bat
@@ -0,0 +1,19 @@
+<<<<<<< HEAD
+
+@echo off
+
+echo %time%
+timeout 5 > NUL
+echo %time%
+
+start F:\RMSSOFT310\mrms_pro.exe
+=======
+
+@echo off
+
+echo %time%
+timeout 5 > NUL
+echo %time%
+
+start F:\RMSSOFT310\mrms_pro.exe
+>>>>>>> 03ee8641c58ed4e4c31fa761e893c2c6a73b20d2
